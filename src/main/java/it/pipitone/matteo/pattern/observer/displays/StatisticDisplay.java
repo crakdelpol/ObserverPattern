fe7b490f09da00249a39ee08c1f952b83ad8075d
Matteo Pipitone <matteo.pipitone@gmail.com>
2020-08-26 22:47:41 +0200

    edits

--- src/main/java/it/pipitone/matteo/pattern/observer/displays/StatisticDisplay.java
+++ src/main/java/it/pipitone/matteo/pattern/observer/displays/StatisticDisplay.java
@@ -1,10 +1,11 @@
 package it.pipitone.matteo.pattern.observer.displays;
 
+
 import it.pipitone.matteo.pattern.observer.Observer;
 
-public class GeneralDisplay implements Observer {
+public class StatisticDisplay implements Observer {
     @Override
     public void update(int temperature, int humidity, int pressure) {
-        System.out.println("Update general display with new Parameters");
+        System.out.println("Update StatisticDisplay display with new Parameters");
     }
 }
